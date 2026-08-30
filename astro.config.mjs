a335fe6129350b661ce794f90e7be8d26300b83c
priteshshah010 <pritesh.shah010@gmail.com>
2026-08-30 17:12:21 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://infinoradental.com',
+  trailingSlash: 'always',
+  build: {
+    assets: '_astro',
+  }
 });
